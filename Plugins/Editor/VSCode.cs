4a7931555176a0e6c6764a3008445e028bb3b478
Rafael Rodrigues Nakano <mseqs@bsd.com.br>
2016-06-02 20:39:59 -0300

    Add vscode insider option
    Adds for windows a option to auto-detect VSCode insider installation folder (if the default one is not installed, if so, set the desired one at the Preferences menu

--- Plugins/Editor/VSCode.cs
+++ Plugins/Editor/VSCode.cs
@@ -394,17 +394,17 @@ namespace dotBunny.Unity
         	/// </summary>
         	static string autodetectCodePath() 
         	{
-        	// FIXME: Need to query possible paths to the installation of VSCode on other OS X
         	string[] possiblePaths =
         	#if UNITY_EDITOR_OSX
         		{
         			"/Applications/Visual Studio Code.app"
         		};
         	#elif UNITY_EDITOR_WIN
-        	// FIXME: Add additional paths which VSCode can be installed on Windows
         		{
         			ProgramFilesx86() + Path.DirectorySeparatorChar + "Microsoft VS Code"
-        			+ Path.DirectorySeparatorChar + "bin" + Path.DirectorySeparatorChar + "code.cmd"
+        			+ Path.DirectorySeparatorChar + "bin" + Path.DirectorySeparatorChar + "code.cmd",
+        			ProgramFilesx86() + Path.DirectorySeparatorChar + "Microsoft VS Code Insiders"
+        			+ Path.DirectorySeparatorChar + "bin" + Path.DirectorySeparatorChar + "code-insiders.cmd"
         		};
         	#else
         		{
@@ -460,8 +460,8 @@ namespace dotBunny.Unity
             	return; //Executable not found. Stop execution
             }
 
-			//Escaping spaces in the name isn't needed in a Windows environment
-			//So for OS X and Linux/Unix, call EscapePathScapes(args) instead of args
+	//Escaping spaces in the name isn't needed in a Windows environment
+	//So for OS X and Linux/Unix, call EscapePathScapes(args) instead of args
 #if UNITY_EDITOR_OSX
             proc.StartInfo.FileName = "open";
 	    proc.StartInfo.Arguments = " -n -b \"com.microsoft.VSCode\" --args " + EscapePathSpaces(args);
